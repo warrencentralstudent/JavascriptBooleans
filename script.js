cf33ad8921ccee7ffc370d9ba26d1ebc49c177bc
Davian Affognon <affogdav000@warren.k12.in.us>
2024-10-03 11:32:51 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function whichButton(number){
+    let element = document.getElementById("result");
+    if(number === 1){
+        element.innerHTML = "1"
+    }
+    if(number === 2){
+        element.innerHTML = "2"
+    }
+}
